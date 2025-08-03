19298f5259830dac9e2e3a5dc5d1e076cd8288cd
SergioCortesN <sergio.cortes.n290399@gmail.com>
2025-08-03 17:14:30 -0600

    Sorteo y visualizacion del amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    if(document.getElementById("amigo").value === ""){
+        alert("Por favor, ingresa un nombre.");
+        return;
+    } else {
+        amigos.push(document.getElementById("amigo").value)
+        document.getElementById("amigo").value = "";
+        mostrarAmigos();
+    }
+}
+
+function mostrarAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    for (let index = 0; index < amigos.length; index++) {
+        lista.innerHTML += `<li>${amigos[index]}</li>`;
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert("No hay amigos para sortear.");
+        return;
+    }else {
+        document.getElementById("listaAmigos").innerHTML = "";
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length-1);
+        document.getElementById("resultado").innerHTML = `el amigo secreto es: ${amigos[indiceAleatorio]}`;
+    }
+}
