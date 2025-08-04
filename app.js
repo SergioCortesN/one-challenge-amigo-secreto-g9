33f06d14d7bb51b9685208b1eb6d0b1654e0285a
SergioCortesN <sergio.cortes.n290399@gmail.com>
2025-08-03 19:31:31 -0600

    Validaciones Agregadas

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let amigos = [];
-
+let mensajeError = "";
 function agregarAmigo(){
-    if(document.getElementById("amigo").value === ""){
-        alert("Por favor, ingresa un nombre.");
+    if(validarNombre() === false){
+        alert(mensajeError);
         return;
     } else {
+        validarNombre() 
         amigos.push(document.getElementById("amigo").value)
         document.getElementById("amigo").value = "";
         mostrarAmigos();
@@ -36,4 +37,31 @@ function reiniciar(){
     document.getElementById("listaAmigos").innerHTML = "";
     document.getElementById("resultado").innerHTML = "";
     document.getElementById("amigo").value = "";
+}
+
+function eliminarAmigo(index){
+    amigos.pop(index);
+    mostrarAmigos();
+}
+
+function validarNombre() {
+    if (document.getElementById("amigo").value === "") {
+        mensajeError = "Por favor, ingresa un nombre.";
+        return false;
+    }else if( amigos.includes(document.getElementById("amigo").value)) {
+        mensajeError = "El nombre ya está en la lista.";
+        return false;
+    } else if (document.getElementById("amigo").value.length < 3) {
+        mensajeError = "El nombre debe tener al menos 3 caracteres.";
+        return false;
+    } else if (document.getElementById("amigo").value.length > 20) {
+        mensajeError = "El nombre no puede tener más de 20 caracteres.";
+        return false;
+    } else if (!/^[a-zA-Z\s]+$/.test(document.getElementById("amigo").value)) {
+        mensajeError = "El nombre solo puede contener letras y espacios.";
+        return false;
+    } else {
+        mensajeError = "";
+        return true;
+    }
 }
